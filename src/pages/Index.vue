ec9268c3e150b853758d62c30ee46cda5abd5685
Vilnite <vilnitekristine@gmail.com>
2020-11-18 15:10:40 +0100

    Landing page image update

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,7 +4,7 @@
     <div class="container sm:pxi-0 mx-auto overflow-x-hidden">
        
       <VacationCard
-        img="/img/Homepage.jpg"
+        img="/img/homePage.png"
         imgAlt="Beach in Cancun"
         eyebrow=""
         title=""
